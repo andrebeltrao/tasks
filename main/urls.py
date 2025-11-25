10cecc64541c4626b3a5434059a2b6323c87f0bb
Daniel Monteiro <daniel.monteiro47420@edu.pe.senac.br>
2025-11-25 11:52:05 -0300

    projeto em andamento
    em construção

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_List, name='task_list'),
+    path('concluidas/', views.tasks_ok, name='tasks_ok'),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from main.models import Task
 
-# Create your views here.
+
+def task_List(request):
+    tarefas = Task.objects.all()
+    context = {
+        'tarefas':tarefas
+    }
+    
+    return render(request, 'tasks/task_list.html', context)
+
+
+
+def tasks_ok(request):
+    
+    tarefas = Task.objects.filter(concluida=1)
+    context = {
+        'tarefas':tarefas
+    }
+    
+    return render(request,'tasks/tasks_ok.html')
